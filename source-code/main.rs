b817a267ffc33d6152c7bbcc2b2ce056a658c277
michal92299 <chudybyczek2137@gmail.com>
2026-05-22 09:43:45 +0200

    Update main.rs

--- source-code/main.rs
+++ source-code/main.rs
@@ -1,12 +1,3 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright 2025 Hammer Contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-
 mod cache;
 mod cli;
 mod db;
